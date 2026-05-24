a85aa66d81d358e2ba67cd3e27d08b88c1a3181c
user <@>
2026-05-24 21:41:16 +0300

    feat(users): implement CRUD and schemas for users

---
+++ users/schemas.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
+
+from pydantic import BaseModel, EmailStr
+
+
+class CreateUser(BaseModel):
+    username: Annotated[str, MinLen(3), MaxLen(30)]
+    email: EmailStr
+
